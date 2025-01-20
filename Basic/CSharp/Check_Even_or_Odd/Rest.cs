cc8a1c415d90d5b451ec222cd053e9bcaf924289
Italo-Thiago <italothiago220@gmail.com>
2025-01-20 12:31:03 -0300

    style: New folder organization

---
+++ Basic/CSharp/Check_Even_or_Odd/Rest.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace Check_Even_or_Odd;
+
+public class Rest
+{
+    public static bool isEven(int n) {
+        return (n % 2 == 0);
+    }
+}
---
+++ Basic/CSharp/Days_to_Seconds/Program.cs
@@ -0,0 +1,7 @@
+﻿//EN: Ask the user to enter a number of days and convert it in seconds
+//PT: Peça ao usuário para inserir um número de dias e converta-o em segundos.
+
+Console.WriteLine("Enter a number of days: ");
+int days = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Seconds: {days * 86400}");
