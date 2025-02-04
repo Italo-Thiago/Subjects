46021200ab01fff1525506fc016ed930b351da73
Italo-Thiago <italothiago220@gmail.com>
2025-02-04 16:23:03 -0300

    chore: update .gitignore

--- Ex/ex.py
+++ Ex/ex.py
@@ -1,32 +1,4 @@
-'''
-Exemplo de uso do return 
-'''
-# Múltiplos valores de retorno
-def fun_multi():
-    name = "Alice"
-    age = 30
-    return name, age
-
-name, age = fun_multi();
-print(name)
-print(age)
-
-# Retornando uma lista
-def fun_list(number):
-    return [number**2, number**3]
-
-res = fun_list(3)
-print(res)
-
-# Retornando outra função
-def fun_1(message):
-    def fun_2():
-        # Using the outer function's message
-        return f"Message: {message}"
-    return fun_2
-
-# Getting the inner function
-fun_3 = fun_1("Hello, World!");
-
-# Calling the inner functions
-print(fun_3())
+li = [lambda arg = x: arg * 10 for x in range(1, 5)]
+for i in li:
+    print(i())
+    # lkdjjflksjlkfjjlaksjdlkajd
