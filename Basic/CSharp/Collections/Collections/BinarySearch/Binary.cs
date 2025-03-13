71b78eca3feed368eec435b1cad484a477ff14da
Italo Thiago <italothiago220@gmail.com>
2025-03-13 13:15:23 -0300

    feat: Adds exemples of Collections in C#

---
+++ Basic/CSharp/Collections/Collections/BinarySearch/Binary.cs
@@ -0,0 +1,67 @@
+﻿namespace Collections.BinarySearch;
+
+public static class Binary
+{
+    public static int Iterative(List<int> list, int item)
+    {
+        int lower = 0;
+        int upper = list.Count - 1;
+        while (lower <= upper)
+        {
+            int middle = (lower + upper) / 2;
+            int element = list[middle];
+            if (element == item)
+                return middle;
+            if (element > item)
+                upper = middle - 1;
+            else
+                lower = middle + 1;
+        }
+
+        return -1;
+    }
+
+    public static int Recursive(List<int> list, int key, int start, int end)
+    {
+        int mid;
+        if (start <= end)
+        {
+            mid = (start + end) / 2;
+            if (key == list[mid])
+                return mid;
+            if (key < list[mid])
+                return Recursive(list, key, start, mid - 1);
+            else
+                return Recursive(list, key, mid + 1, end);
+        }
+        return -1;
+    }
+    
+    // https://learn.microsoft.com/pt-br/dotnet/api/system.array.binarysearch?view=net-8.0
+    public static void FindMyObject(Array myArr, object myObject)
+    {
+        int myIndex = Array.BinarySearch(myArr, myObject);
+        if (myIndex < 0)
+            Console.WriteLine("The object to search for ({0}) is not found. The next larger object is at index {1}.", myObject, ~myIndex);
+        else
+            Console.WriteLine("The object to search for ({0}) is at index {1}.", myObject, myIndex);
+    }
+
+    public static void PrintValues(Array myArr)
+    {
+        int i = 0;
+        int cols = myArr.GetLength(myArr.Rank - 1);
+        foreach (object o in myArr)
+        {
+            if (i < cols)
+                i++;
+            else
+            {
+                Console.WriteLine();
+                i = 1;
+            }
+            Console.Write("\t{0}", o);
+        }
+        Console.WriteLine();
+    }
+}
---
+++ Basic/CSharp/Collections/Collections/Program.cs
@@ -0,0 +1,46 @@
+﻿using Collections.BinarySearch;
+
+List<int> zero = [];
+List<int> list = PopulationList(zero, 50);
+ShowList(list);
+int number = 40;
+Console.WriteLine($"\nIterative = {Binary.Iterative(list, number)}");
+Console.WriteLine($"\nRecursive = {Binary.Recursive(list, number, 0, list.Count)}");
+
+Array myIntArray = Array.CreateInstance(typeof(int),5);
+
+myIntArray.SetValue(8, 0);
+myIntArray.SetValue(2, 1);
+myIntArray.SetValue(6, 2);
+myIntArray.SetValue(3, 3);
+myIntArray.SetValue(7, 4);
+
+Array.Sort(myIntArray);
+
+Console.WriteLine("The int array contains the following: ");
+Binary.PrintValues(myIntArray);
+
+object myObjectOdd = 1;
+Binary.FindMyObject(myIntArray, myObjectOdd);
+
+object myObjectEven = 6;
+Binary.FindMyObject(myIntArray, myObjectEven);
+                
+List<int> PopulationList(List<int> list, int size) 
+{
+    for (int i = 1; i < size; i++)
+    {
+        list.Add(i);
+    }
+
+    return list;
+}
+
+void ShowList(List<int> list)
+{
+    foreach (var element in list)
+    {
+       Console.Write($" {element}"); 
+    } 
+}
+
